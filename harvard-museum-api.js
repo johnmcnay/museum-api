36e5b12984c386ea3ffea0fcf0f1c31a16910480
John McNay <John.McNay@improving.com>
2021-05-27 07:54:47 -0500

    height is set instead of width; still needs text/caption

--- harvard-museum-api.js
+++ harvard-museum-api.js
@@ -8,16 +8,19 @@ form.addEventListener("submit", e => {
     fetch("https://api.harvardartmuseums.org/image?apikey=6c51ae5f-b2b0-4be1-8d80-6225c80ec75c&q=" + input.value)
         .then(response => response.json())
         .then(data => {
+
+            target.innerHTML = "";
+
             for (let record of data.records) {
                 console.log(record);
                 let container = document.createElement("div");
                 let img = document.createElement("img");
+                let p = document.createElement("div");
                
                 container.style.display = "inline-block";
-                /*container.style.width = "200px";
-                container.style.height = "500px";*/
 
                 img.src = record.baseimageurl;
+                //p.textContent = record.description;
                
                 container.appendChild(img);
 
